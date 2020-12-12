cc7fe98152c7269936ca61050caef15d3b93e39a
Tyler Kontra <tyler@tylerkontra.com>
2020-12-12 14:50:50 -0800

    access logging, fix template

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,5 +4,6 @@ bind = "0.0.0.0:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "gevent"
 worker_connections = 500 
-loglevel = "debug"
+loglevel = "INFO"
 capture_output = True
+accesslog = "-"
